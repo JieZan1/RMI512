9c2a3387f7d5c3ea5eccc552ab67df58382903a6
JieZan1 <144639492+JieZan1@users.noreply.github.com>
2025-10-01 15:45:11 -0400

    Add files via upload

---
+++ MiddleWare/MiddleWare/Common/Customer.java
@@ -0,0 +1,206 @@
+package MiddleWare.Common;
+
+import java.util.*;
+
+public class Customer {
+    private int cid;
+    private List<HashMap<Integer, Integer>> flights = new ArrayList<>();
+    private List<HashMap<String, Integer>> cars = new ArrayList<>();
+    private List<HashMap<String, Integer>> rooms = new ArrayList<>();
+
+    public Customer(int id) {
+        cid = id;
+        // Initialize with empty HashMaps
+        flights.add(new HashMap<>());
+        cars.add(new HashMap<>());
+        rooms.add(new HashMap<>());
+    }
+
+//     
+//    public List<Integer> getFlights() {
+//        List<Integer> flightList = new ArrayList<>();
+//        for (HashMap<Integer, Integer> flightMap : flights) {
+//            for (Map.Entry<Integer, Integer> entry : flightMap.entrySet()) {
+//                Integer flightNum = entry.getKey();
+//                Integer quantity = entry.getValue();
+//                for (int i = 0; i < quantity; i++) {
+//                    flightList.add(flightNum);
+//                }
+//            }
+//        }
+//        return Collections.unmodifiableList(flightList);
+//    }
+//
+//     
+//    public List<String> getCars() {
+//        List<String> carList = new ArrayList<>();
+//        for (HashMap<String, Integer> carMap : cars) {
+//            for (Map.Entry<String, Integer> entry : carMap.entrySet()) {
+//                String location = entry.getKey();
+//                Integer quantity = entry.getValue();
+//                for (int i = 0; i < quantity; i++) {
+//                    carList.add(location);
+//                }
+//            }
+//        }
+//        return Collections.unmodifiableList(carList);
+//    }
+//
+//     
+//    public List<String> getRooms() {
+//        List<String> roomList = new ArrayList<>();
+//        for (HashMap<String, Integer> roomMap : rooms) {
+//            for (Map.Entry<String, Integer> entry : roomMap.entrySet()) {
+//                String location = entry.getKey();
+//                Integer quantity = entry.getValue();
+//                for (int i = 0; i < quantity; i++) {
+//                    roomList.add(location);
+//                }
+//            }
+//        }
+//        return Collections.unmodifiableList(roomList);
+//    }
+
+     
+    public boolean addFlight(int flight) {
+        if (!flights.isEmpty()) {
+            HashMap<Integer, Integer> flightMap = flights.get(0);
+            flightMap.put(flight, flightMap.getOrDefault(flight, 0) + 1);
+            return true;
+        }
+        return false;
+    }
+
+     
+    public boolean addCar(String car) {
+        if (!cars.isEmpty()) {
+            HashMap<String, Integer> carMap = cars.get(0);
+            carMap.put(car, carMap.getOrDefault(car, 0) + 1);
+            return true;
+        }
+        return false;
+    }
+
+     
+    public boolean addRoom(String room) {
+        if (!rooms.isEmpty()) {
+            HashMap<String, Integer> roomMap = rooms.get(0);
+            roomMap.put(room, roomMap.getOrDefault(room, 0) + 1);
+            return true;
+        }
+        return false;
+    }
+
+     
+    public int removeFlight(int flight) {
+        int totalRemoved = 0;
+        for (HashMap<Integer, Integer> flightMap : flights) {
+            if (flightMap.containsKey(flight)) {
+                totalRemoved += flightMap.get(flight);
+                flightMap.remove(flight);
+            }
+        }
+        if (totalRemoved > 0) {
+            System.out.println("Removed " + totalRemoved + " flight(s) with number " + flight);
+        }
+        return totalRemoved;
+    }
+
+     
+    public int removeCar(String car) {
+        int totalRemoved = 0;
+        for (HashMap<String, Integer> carMap : cars) {
+            if (carMap.containsKey(car)) {
+                totalRemoved += carMap.get(car);
+                carMap.remove(car);
+            }
+        }
+        if (totalRemoved > 0) {
+            System.out.println("Removed " + totalRemoved + " car(s) at location " + car);
+        }
+        return totalRemoved;
+    }
+
+     
+    public int removeRoom(String room) {
+        int totalRemoved = 0;
+        for (HashMap<String, Integer> roomMap : rooms) {
+            if (roomMap.containsKey(room)) {
+                totalRemoved += roomMap.get(room);
+                roomMap.remove(room);
+            }
+        }
+        if (totalRemoved > 0) {
+            System.out.println("Removed " + totalRemoved + " room(s) at location " + room);
+        }
+        return totalRemoved;
+    }
+
+     
+    public int getFlightCount(int flight) {
+        int totalCount = 0;
+        for (HashMap<Integer, Integer> flightMap : flights) {
+            totalCount += flightMap.getOrDefault(flight, 0);
+        }
+        return totalCount;
+    }
+
+     
+    public int getCarCount(String car) {
+        int totalCount = 0;
+        for (HashMap<String, Integer> carMap : cars) {
+            totalCount += carMap.getOrDefault(car, 0);
+        }
+        return totalCount;
+    }
+
+     
+    public int getRoomCount(String room) {
+        int totalCount = 0;
+        for (HashMap<String, Integer> roomMap : rooms) {
+            totalCount += roomMap.getOrDefault(room, 0);
+        }
+        return totalCount;
+    }
+
+     
+    public int getCustomerId() {
+        return cid;
+    }
+
+     
+    public Map<Integer, Integer> getFlightReservations() {
+        Map<Integer, Integer> allFlights = new HashMap<>();
+        for (HashMap<Integer, Integer> flightMap : flights) {
+            for (Map.Entry<Integer, Integer> entry : flightMap.entrySet()) {
+                allFlights.put(entry.getKey(),
+                        allFlights.getOrDefault(entry.getKey(), 0) + entry.getValue());
+            }
+        }
+        return allFlights;
+    }
+
+     
+    public Map<String, Integer> getCarReservations() {
+        Map<String, Integer> allCars = new HashMap<>();
+        for (HashMap<String, Integer> carMap : cars) {
+            for (Map.Entry<String, Integer> entry : carMap.entrySet()) {
+                allCars.put(entry.getKey(),
+                        allCars.getOrDefault(entry.getKey(), 0) + entry.getValue());
+            }
+        }
+        return allCars;
+    }
+
+     
+    public Map<String, Integer> getRoomReservations() {
+        Map<String, Integer> allRooms = new HashMap<>();
+        for (HashMap<String, Integer> roomMap : rooms) {
+            for (Map.Entry<String, Integer> entry : roomMap.entrySet()) {
+                allRooms.put(entry.getKey(),
+                        allRooms.getOrDefault(entry.getKey(), 0) + entry.getValue());
+            }
+        }
+        return allRooms;
+    }
+}
---
+++ Server/Server/DistributedCommon/DistributedResourceManager.java
@@ -0,0 +1,225 @@
+package Server.DistributedCommon;
+
+import Server.Common.*;
+import Server.Interface.IResourceManager;
+
+import java.rmi.RemoteException;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Vector;
+
+
+
+public class DistributedResourceManager implements IResourceManager {
+    protected String m_name = "";
+
+    protected HashMap<Integer, DistributedFlight>  m_flights;
+    protected HashMap<String, DistributedRoom>  m_rooms;
+    protected HashMap<String, DistributedCar>  m_car;
+
+    public DistributedResourceManager(String p_name)
+    {
+        m_name = p_name;
+    }
+
+    // Reads a data item
+
+    public boolean addFlight(int flightNum, int flightSeats, int flightPrice) throws RemoteException
+    {
+        Trace.info("RM::addFlight(" + flightNum + ", " + flightSeats + ", $" + flightPrice + ") called");
+        DistributedFlight curObj = m_flights.get(flightNum);
+        if (curObj == null)
+        {
+            // Doesn't exist yet, add it
+            DistributedFlight newObj = new DistributedFlight(flightNum, flightSeats, flightPrice);
+            m_flights.put(flightNum, newObj);
+            Trace.info("RM::addFlight() created new flight " + flightNum + ", seats=" + flightSeats + ", price=$" + flightPrice);
+        }
+        else
+        {
+            // Add seats to existing flight and update the price if greater than zero
+            curObj.setSeats(curObj.getSeats() + flightSeats);
+            if (flightPrice > 0)
+            {
+                curObj.setPrice(flightPrice);
+            }
+            m_flights.put(flightNum, curObj);
+            Trace.info("RM::addFlight() modified existing flight " + flightNum + ", seats=" + curObj.getSeats() + ", price=$" + flightPrice);
+        }
+        return true;
+    }
+
+    public boolean addCars(String location, int numCars, int price) throws RemoteException {
+        Trace.info("RM::addCars(" + location + ", " + numCars + ", $" + price + ") called");
+        DistributedCar curObj = m_car.get(location);
+        if (curObj == null) {
+            // Doesn't exist yet, add it
+            DistributedCar newObj = new DistributedCar(location, numCars, price);
+            m_car.put(location, newObj);
+            Trace.info("RM::addCars() created new cars at " + location + ", count=" + numCars + ", price=$" + price);
+        } else {
+            // Add cars to existing location and update the price if greater than zero
+            curObj.setCars(curObj.getCars() + numCars);
+            if (price > 0) {
+                curObj.setPrice(price);
+            }
+            m_car.put(location, curObj);
+            Trace.info("RM::addCars() modified cars at " + location + ", count=" + curObj.getCars() + ", price=$" + price);
+        }
+        return true;
+    }
+
+
+    public boolean addRooms(String location, int numRooms, int price) throws RemoteException {
+        Trace.info("RM::addRooms(" + location + ", " + numRooms + ", $" + price + ") called");
+        DistributedRoom curObj = m_rooms.get(location);
+        if (curObj == null) {
+            // Doesn't exist yet, add it
+            DistributedRoom newObj = new DistributedRoom(location, numRooms, price);
+            m_rooms.put(location, newObj);
+            Trace.info("RM::addRooms() created new rooms at " + location + ", count=" + numRooms + ", price=$" + price);
+        } else {
+            // Add rooms to existing location and update the price if greater than zero
+            curObj.setRooms(curObj.getRooms() + numRooms);
+            if (price > 0) {
+                curObj.setPrice(price);
+            }
+            m_rooms.put(location, curObj);
+            Trace.info("RM::addRooms() modified rooms at " + location + ", count=" + curObj.getRooms() + ", price=$" + price);
+        }
+        return true;
+    }
+
+
+    public boolean deleteFlight(int flightNum) throws RemoteException
+    {
+        if (m_flights.containsKey(flightNum)){
+           m_flights.remove(flightNum);
+           return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean deleteCars(String location) throws RemoteException
+    {
+        if (m_car.containsKey(location)){
+            m_car.remove(location);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean deleteRooms(String location) throws RemoteException
+    {
+        if (m_rooms.containsKey(location)){
+            m_rooms.remove(location);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public int queryFlight(int flightNum) throws RemoteException {
+        int value = 0;
+        DistributedFlight curObj = m_flights.get(flightNum);
+        if (curObj != null) {
+            value = curObj.getSeats();
+        }
+        return value;
+    }
+
+    public int queryCars(String location) throws RemoteException {
+        int value = 0;
+        DistributedCar curObj = m_car.get(location);
+        if (curObj != null) {
+            value = curObj.getCars();
+        }
+        return value;
+    }
+
+    public int queryRooms(String location) throws RemoteException {
+        int value = 0;
+        DistributedRoom curObj = m_rooms.get(location);
+        if (curObj != null) {
+            value = curObj.getRooms();
+        }
+        return value;
+    }
+
+    public int queryFlightPrice(int flightNum) throws RemoteException {
+        int price = 0;
+        DistributedFlight curObj = m_flights.get(flightNum);
+        if (curObj != null) {
+            price = curObj.getPrice();
+        }
+        return price;
+    }
+
+    public int queryCarsPrice(String location) throws RemoteException {
+        int price = 0;
+        DistributedCar curObj = m_car.get(location);
+        if (curObj != null) {
+            price = curObj.getPrice();
+        }
+        return price;
+    }
+
+    public int queryRoomsPrice(String location) throws RemoteException {
+        int price = 0;
+        DistributedRoom curObj = m_rooms.get(location);
+        if (curObj != null) {
+            price = curObj.getPrice();
+        }
+        return price;
+    }
+
+    public String queryCustomerInfo(int customerID) throws RemoteException
+    {
+        return "";
+    }
+
+    public int newCustomer() throws RemoteException
+    {
+        return -1;
+    }
+
+    public boolean newCustomer(int customerID) throws RemoteException
+    {
+        return false;
+    }
+
+    public boolean deleteCustomer(int customerID) throws RemoteException
+    {
+        return false;
+    }
+
+    public boolean reserveFlight(int customerID, int flightNum) throws RemoteException
+    {
+        return false;
+    }
+
+    public boolean reserveCar(int customerID, String location) throws RemoteException
+    {
+        return false;
+    }
+
+    public boolean reserveRoom(int customerID, String location) throws RemoteException
+    {
+        return false;
+    }
+
+    public boolean bundle(int customerId, Vector<String> flightNumbers, String location, boolean car, boolean room) throws RemoteException
+    {
+        return false;
+    }
+
+    public String getName() throws RemoteException
+    {
+        return m_name;
+    }
+}
