1d162c5d1ecc62a205d464b8fa86b51dd58198d5
Jie Zan <jie.zan@mail.mcgill.ca>
2025-10-02 10:31:30 -0400

    changed sscript

--- MiddleWare/run_middle.sh
+++ MiddleWare/run_middle.sh
@@ -1,4 +1,4 @@
-#../Server/run_rmi.sh > /dev/null
+../Server/run_rmi.sh > /dev/null
 
 # echo "Edit file run_middleware.sh to include instructions for launching the middleware"
 echo '  $1 - server name of Flights'
@@ -6,6 +6,6 @@ echo '  $2 - server name of Cars'
 echo '  $3 - server name of Rooms'
 
 #adjust port number if necessary
-rmiregistry -J-Djava.rmi.server.useCodebaseOnly=false 1045
+#rmiregistry -J-Djava.rmi.server.useCodebaseOnly=false 1045
 
 java -cp .:../Server/RMIInterface.jar -Djava.rmi.server.codebase=file:../Server/RMIInterface.jar MiddleWare.RMI.RMIMiddleware $1 $2 $3
